4302ffb739a2ee5155f8b8d8144e8a23b8a0e9b5
skygnite <skygnite@gmail.com>
2019-07-20 18:50:23 -0500

    reset board added, start player rig

---
+++ Assets/InputController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    //detect a tap on a button, then fire function that spawns board
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/ResetBoardButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetBoardButton : MonoBehaviour
+{
+    public GameObject gameBoard;
+    public Transform resetTransform;
+    private void OnMouseDown()
+    {
+        gameBoard.transform.position = resetTransform.position;
+        gameBoard.SetActive(true);
+    }
+}
