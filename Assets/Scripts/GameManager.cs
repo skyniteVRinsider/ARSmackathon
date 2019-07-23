8ede3b57660a3da64e3b07a9ef78d06c2e0534cf
Avi Marks <alanmarks@Avis-MacBook-Pro.local>
2019-07-22 22:14:33 -0500

    architecture

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+Enumeration GameState{PlacingBoard, InGame};
+public class GameManager : MonoBehaviour
+{
+    bool boardIsPlaced = false;
+    public static GameManager thisScene = null;
+
+    public MenuManager menuManager;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //singletoning
+        if (thisScene == null)
+        {
+            thisScene = this; 
+        } else if (thisScene != null)
+        {
+            Destroy(this);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
